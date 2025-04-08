02ce07b51a90dc266f3905b4a02cb5eba096b76a
Anton Chernov <tony.pitchblack@yandex.ru>
2025-04-08 14:12:24 +0300

    fix setup.sh for xgb

--- competitors-xgb/setup.sh
+++ competitors-xgb/setup.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-# micromamba create -n competitors-xgb \
-#     python=3.12 cython youtokentome -y
-# pip install -r requirements.txt
-
 set -e  # Exit immediately if a command exits with a non-zero status
 
 # Initialize micromamba for the current shell using the shell hook.
 eval "$(micromamba shell hook -s bash)"
 
-# Activate your environment (replace "myenv" with your actual environment name)
-micromamba activate competitors-xgb
+# Create env & install critical deps first
+micromamba create -n competitors-xgb \
+    python=3.12 cython youtokentome -y
 
-# Install packages from requirements.txt using pip
+# Install remaining deps
+micromamba activate competitors-xgb
 pip install -r requirements.txt
 
 echo "Setup complete!"
